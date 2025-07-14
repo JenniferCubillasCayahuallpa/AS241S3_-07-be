de0277cc0dbefc651f264416f874e6cac63b4729
Jennifer Alejandra Cubillas <155853100+JenniferCubillasCayahuallpa@users.noreply.github.com>
2025-07-14 07:57:40 +0000

    Cambioooooos, go cambioooos

--- src/main/java/vg/jennifer/cubillas/hackathon/config/CorsConfig.java
+++ src/main/java/vg/jennifer/cubillas/hackathon/config/CorsConfig.java
@@ -9,7 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:4200", "https://tudominio.com")
+                .allowedOrigins(
+                    "http://localhost:4200",
+                    "https://tudominio.com",
+                    "https://friendly-disco-5gg7qjq74xj937gq6-4200.app.github.dev"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
